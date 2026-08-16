3851b9706e23d48df3a1ad719de5ec98350ad295
FoxParadeAFK <s.kittwong004@gmail.com>
2026-08-16 14:53:51 +0100

    Move state + idle state

--- lappland-legato/scripts/player/player.gd
+++ lappland-legato/scripts/player/player.gd
@@ -1,21 +1,30 @@
 class_name Player extends CharacterBody2D
 
 var current_state: State
+var idle_state: IdleState
+var move_state: MoveState
 func transition_state(_transitioning_state: State) -> void:
 	if current_state != null: current_state.exit()
 	
 	print("Time: %5s State: %s" % [Time.get_ticks_msec(), _transitioning_state.animation_name])
 	current_state = _transitioning_state
 	current_state.enter()
+
+func get_state(_state_path: NodePath, _animation_name: String) -> State:
+	var state: State = get_node_or_null(_state_path)
+	if state != null: state.engage(self, _animation_name)
 	
-var idle_state: IdleState
+	return state
+	
+var horizontal_input: float
 
 func _ready() -> void:
-	idle_state = get_node_or_null("FiniteStateMachine/IdleState")
-	idle_state.engage(self, "idle")
+	idle_state = get_state("FiniteStateMachine/IdleState", "idle")
+	move_state = get_state("FiniteStateMachine/MoveState", "move")
 	transition_state(idle_state)
 
 func _physics_process(_delta: float) -> void:
+	horizontal_input = Input.get_axis("ui_left", "ui_right")
 	current_state.physics_update(_delta)
 	
 	move_and_slide()
