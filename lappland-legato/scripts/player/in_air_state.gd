04ede3ccfc797b3a52f305d62b2988d9dc05cdc9
FoxParadeAFK <s.kittwong004@gmail.com>
2026-08-17 00:26:03 +0100

    Fairly delicate vertical release buffer

--- lappland-legato/scripts/player/in_air_state.gd
+++ lappland-legato/scripts/player/in_air_state.gd
@@ -3,8 +3,14 @@ class_name InAirState extends State
 func enter() -> void: pass
 
 func physics_update(_delta: float) -> void:
+	var gravity: int = player.GRAVITY
+	if sign(player.velocity.y) == -1 and player.vertical_input_released:
+		player.velocity.y *= 0.6
+		player.vertical_input_released = false
+		player.vertical_input_released_buffer_timer.stop()
+	
 	player.velocity.x = player.horizontal_input * player.HORIZONTAL_VELOCITY
-	player.velocity.y += player.GRAVITY * _delta
+	player.velocity.y += gravity * _delta
 	
 	if player.is_on_floor() and player.horizontal_input == 0: player.transition_state(player.idle_state)
 	elif player.is_on_floor() and player.horizontal_input != 0: player.transition_state(player.move_state)
--- lappland-legato/scripts/player/player.gd
+++ lappland-legato/scripts/player/player.gd
@@ -21,8 +21,10 @@ func get_state(_state_path: NodePath, _animation_name: String) -> State:
 enum {HORIZONTAL_VELOCITY = 100, VERTICAL_VELOCITY = 140, GRAVITY = 650}
 var horizontal_input: float
 var vertical_input: bool
+var vertical_input_released: bool
 
 var vertical_input_buffer_timer: Timer
+var vertical_input_released_buffer_timer: Timer
 
 func _ready() -> void:
 	idle_state = get_state("FiniteStateMachine/IdleState", "idle")
@@ -33,10 +35,13 @@ func _ready() -> void:
 	
 	vertical_input_buffer_timer = get_node_or_null("VerticalInputBufferTimer")
 	vertical_input_buffer_timer.timeout.connect(func() -> void: vertical_input = false)
+	vertical_input_released_buffer_timer = get_node_or_null("VerticalInputReleasedBufferTimer")
+	vertical_input_released_buffer_timer.timeout.connect(func() -> void: vertical_input_released = false)
 
 func _physics_process(_delta: float) -> void:
 	horizontal_input = Input.get_axis("ui_left", "ui_right")
 	vertical_input_buffer(Input.is_action_just_pressed("ui_accept"))
+	vertical_input_released_buffer(Input.is_action_just_released("ui_accept"))
 	current_state.physics_update(_delta)
 	
 	move_and_slide()
@@ -46,3 +51,9 @@ func vertical_input_buffer(_vertical_input: bool) -> void:
 	
 	vertical_input = true
 	vertical_input_buffer_timer.start()
+	
+func vertical_input_released_buffer(_vertical_input_released: bool) -> void:
+	if not _vertical_input_released or vertical_input_released_buffer_timer.time_left != 0: return
+	
+	vertical_input_released = true
+	vertical_input_released_buffer_timer.start()
