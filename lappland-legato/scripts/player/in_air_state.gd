e20959be6acb0a01855696fad8da85dc9310b986
FoxParadeAFK <s.kittwong004@gmail.com>
2026-08-17 14:47:19 +0100

    Jump hovering time

--- lappland-legato/scripts/player/in_air_state.gd
+++ lappland-legato/scripts/player/in_air_state.gd
@@ -3,11 +3,13 @@ class_name InAirState extends State
 func enter() -> void: pass
 
 func physics_update(_delta: float) -> void:
-	var gravity: int = player.GRAVITY
+	var gravity: float = player.GRAVITY
 	if sign(player.velocity.y) == -1 and player.vertical_input_released:
 		player.velocity.y *= 0.6
 		player.vertical_input_released = false
 		player.vertical_input_released_buffer_timer.stop()
+	if sign(player.velocity.y) == -1 and player.velocity.y >= -45:
+		gravity *= 0.35
 	
 	player.velocity.x = player.horizontal_input * player.HORIZONTAL_VELOCITY
 	player.velocity.y += gravity * _delta
